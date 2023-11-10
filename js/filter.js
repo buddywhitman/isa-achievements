38d3690f8c3cb4a0c576464fd6364a19a0696380
buddywhitman <pulkitindia5@gmail.com>
2023-11-11 00:29:07 +0530

    final cleanup

--- js/filter.js
+++ js/filter.js
@@ -36,30 +36,6 @@ function generatePost(post) {
 
     const article = document.createElement('article');
     article.classList.add('post');
-    // article.innerHTML = `
-    //                 <div class="post__meta">
-    //                     <div class="post__tag--container">
-    //                     ${post.meta.tags.map((tag) =>
-    //     `<span class="post__tag">${tag}</span>`
-    // ).join('')}
-    //                     </div>
-    //                     <p class="post__date">${returnPostDate(new Date(post.meta.date))}</p>
-    //                 </div>
-    //                 <div class="box" data-aos="fade-up">
-    //                     <img class="image" width="55"
-    //                         src="https://i.picsum.photos/id/55/200/300.jpg?hmac=VjTl-6Y6NNyUWof_G17-KlocVl0QuUoxpir1beSTl8A" alt="Random image">
-    //                     <div class="content">
-    //                         <div class="price">
-    //                             <p class="post__author--name">${post.user.name[0].firstName} ${post.user.name[1].lastName}</p>
-    //                         </div>
-    //                         <p class="post__body">
-    //                             ${post.summary}
-    //                         </p>
-    //                         <a href="#" class="btn">Read More</a>
-    //                     </div>
-    //                 </div>
-                
-    // `;
 
     var randomNumber = Math.floor(Math.random() * 6) + 1;
 
@@ -111,6 +87,3 @@ document.querySelector('.btn--view').addEventListener('click', viewMorePosts)
 
 // filter for search
 search.addEventListener('keyup', filterPosts);
-
-
-
